705ee3c6d29517a39793202039626e34a9bc15b3
wutaga <129993611+wutaga@users.noreply.github.com>
2024-03-05 21:59:32 +0300

    Update comparator_int.java

--- comparator_int.java
+++ comparator_int.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 class comparator_int {
     public static void main(String[ ] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(); int b = sc.nextInt();
-        if (a > b){System.out.println(a+">"+b);}
-        else if (b > a){System.out.println(a+"<"+b);}
-        else {System.out.println(a+"="+b);}
+        
+        int a = sc.nextInt(); 
+        int b = sc.nextInt();
+        
+        if (a > b)
+        {
+            System.out.println(a+">"+b);
+        }
+        else if (b > a)
+        {
+            System.out.println(a+"<"+b);
+        }
+        else 
+        {
+            System.out.println(a+"="+b);
+        }
+        
         System.out.println((a+b)+" "+(a-b)+" "+(a*b)+" "+(a/b));
     }
 }
