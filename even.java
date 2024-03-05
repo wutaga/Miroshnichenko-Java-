83935bafdbf64a23bbfcdfe1baa0e2c3109d8ed6
wutaga <129993611+wutaga@users.noreply.github.com>
2024-03-05 22:01:51 +0300

    Update even.java

--- even.java
+++ even.java
@@ -2,8 +2,11 @@ class even {
     public static void main(String[ ] args) {
         int [] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.print("Четные элементы массива : ");
-        for (int i = 0; i < num.length; i++) {
-            if(num[i]%2 == 0){
+        
+        for (int i = 0; i < num.length; i++) 
+        {
+            if(num[i]%2 == 0)
+            {
                 System.out.print(num[i] + ", ");
             }
         }  
